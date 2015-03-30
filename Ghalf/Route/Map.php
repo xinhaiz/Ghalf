a5edbe55e460eee3dd5a1c60b276c667802a8500
Gsinhi <xinhai.z@gsinhi.com>
2015-03-30 14:13:15 +0800

    Updated, 内置路由map调整部分代码

--- Ghalf/Route/Map.php
+++ Ghalf/Route/Map.php
@@ -4,15 +4,15 @@ namespace Ghalf\Route;
 
 class Map implements \Ghalf\Interfaces\Router {
 
-    protected $_isAction  = true;
+    protected $_actionPrefer = true;
     protected $_delimeter = null;
 
-    public function __construct($isControlled = true, $delimeter = '_') {
-        $this->_isAction  = (bool) $isControlled;
-        $this->_delimeter = (string) $delimeter;
+    public function __construct($controllerPrefer = false, $delimeter = '_') {
+        $this->_actionPrefer = !$controllerPrefer;
+        $this->_delimeter      = (string)$delimeter;
     }
 
-    public function route(\Ghalf\RequestAbstract $request, \Ghalf\Router $router) {
+    public function route(\Ghalf\RequestAbstract $request) {
         $uri = $request->getRequestUri();
 
         if (strlen($this->_delimeter) === 1) {
@@ -29,7 +29,7 @@ class Map implements \Ghalf\Interfaces\Router {
 
         $request->setModuleName(null);
 
-        if ($this->_isAction === true) {
+        if ($this->_actionPriority === true) {
             $request->setActionName($ctl_route);
         } else {
             $request->setControllerName($ctl_route);
@@ -38,5 +38,5 @@ class Map implements \Ghalf\Interfaces\Router {
         $request->setRouted(true);
         return true;
     }
-
+    
 }
